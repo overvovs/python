60d8a2611f23aecf96589a4536ffa22cc619cdff
Andrey Gulyayko <a.gulyayko@yandex.ru>
2019-10-24 19:01:24 +0400

    home work 24.10.19

---
+++ shjfhfj.py
@@ -0,0 +1,6 @@
+for x in range(2, 101, 2):
+    print(x)
+
+
+for x in range(1, 101 , 2):
+    print(x)
---
+++ table.py
@@ -0,0 +1,14 @@
+for x in range(2, 10):
+    for y in range(10):
+        print(x, "*", y, "=", x * y)
+
+x = 2
+while x < 10:
+    y = 0
+    while y < 10:
+        print(x, "*", y, "=", x * y)
+        y += 1
+    x += 1
+    
+
+
